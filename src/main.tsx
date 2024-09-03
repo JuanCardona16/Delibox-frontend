59b3873997d329cf57b7b6071dfc8b14bf3745ca
Juan Cardona <jcardonabageth@gmail.com>
2024-09-02 23:21:27 -0500

    config: add new structure folder for project.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './config/Application.tsx'
+import './styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
